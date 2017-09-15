26c6b4df62a58e4392371d681be547ebdb4928c9
Troy Dowling <troy@troydowling.org>
2017-09-14 20:03:29 -0700

    Cleaned up commenting and some code detritus
    - Guess the last commit wasn't my last after all.
    - Added a few comments.
    - Removed code from the template that wasn't used.

--- lab1.cpp
+++ lab1.cpp
@@ -14,46 +14,43 @@
 //
 // 301216033
 
-// Define call time periods in 24h (Prime time, off-peak, and weekend)
-#define T_PRIME_S 8
-#define T_PRIME_P 18
-// Define call time period rates
+// Define call time periods in 24h (Prime time, off-peak, and weekend).
+#define T_PRIME_S 8     // Prime-time starts at 08:00
+#define T_PRIME_P 18    // Ends at 18:00
+// Define call time period rates (cents/minute).
 const double R_PRIME = 0.40;
 const double R_OFFPEAK = 0.25;
 const double R_WEEKEND = 0.15;
 
-#include <iostream>
-#include <iomanip>
-#include <cstdlib>
-#include <vector> 
-#include <map>
-#include <string>
-#include <algorithm>
+#include <iostream>     // Basic IO
+#include <iomanip>      // setprecision() and fixed()
+#include <cstdlib>      // atoi()
+#include <vector>       // Vectors of call prices
+#include <map>          // Map between day string and weekend membership
+#include <string>       // Strings
+#include <algorithm>    // transform() for down-casing
 
-using namespace std;
+using namespace std;    // Given in template, so don't remove I guess.
 
-// The total bill the customer receives
+// The total bill the customer receives.
 typedef struct Bill{
     double totalCost;           // What the customer is required to pay
     int numCalls;               // The number of individual calls made
     vector<double> callCosts;   // A list of what each call cost
 } Bill;
 
-// An individual call made by the customer
+// An individual call made by the customer.
 typedef struct Call{
     int duration;               // The duration of the call made
     double rate;                // The rate at which this call was billed
 } Call;
-
-void calculateCost(int a, int b);
 	
 int main()
 {
-    // Create a running bill for the customer
+    // Create a running bill for the customer.
     Bill bill = {0, 0};
 
-    // Map human-readable weekdays to billing booleans
-    // Since there are only seven days to consider
+    // Map human-readable weekdays to billing booleans.
     map <string, bool> billingDays;
     billingDays["mo"] = false;
     billingDays["tu"] = false;
@@ -63,40 +60,42 @@ int main()
     billingDays["sa"] = true;
     billingDays["su"] = true;
 
-    int terminator = -1;
+    string termStr = "quit";
     string day;
     string time;
     int duration;
-    cout << "Enter \"" << terminator <<
+    cout << "Enter \"" << termStr <<
         "\" for the day of week to terminate loop." << endl;
     do
     {
         // Prompt user for WK, 24:00, and int(duration).
-        cout << endl << "Day of week: " << flush;
+        cout << endl << "Day of week (or \"quit\"): " << flush;
         cin >> day;
-        if(atoi(day.c_str()) == -1)
+        // First transform the string to lower case
+        transform(day.begin(), day.end(), day.begin(), ::tolower);
+        if(day.compare(termStr) == 0)
             break;
         cout << "Time: " << flush;
         cin >> time;
         cout << "Duration: " << flush;
         cin >> duration;
 
-        // Extract hour from time HH:xx
+        // Extract hour from time HH:xx.
         int index = time.find(":");
         string hour = time.substr(0, index);
 
-        // Initialise Call struct
+        // Declare Call struct.
         Call c;
 
-        // Set the call's billing rate depending on the day and time
-        // First transform the string to lower case
-        transform(day.begin(), day.end(), day.begin(), ::tolower);
+        // Set the call's billing rate depending on the day and time.
+        // Map lookup for weekend membership boolean.
         if(billingDays[day])
             // Weekend
             c.rate = R_WEEKEND;
         else
         {
             // Weekday
+            // Check if it's primetime or not.
             int ih = atoi(hour.c_str());
             if(ih < T_PRIME_S || ih > T_PRIME_P)
                 c.rate = R_OFFPEAK;
@@ -104,10 +103,10 @@ int main()
                 c.rate = R_PRIME;
         }
 
-        // Set the call duration
+        // Set the call duration.
         c.duration = duration;
 
-        // Calculate cost of this call
+        // Calculate cost of this call.
         double charge = c.rate * c.duration;
 
         // Add it to the customer's bill
@@ -118,7 +117,7 @@ int main()
     } while(true); // Break condition handled during day input
 
     cout << fixed << setprecision(2);   // Use fixed-point two decimal places
-    cout << "Total bill: $" << bill.totalCost << endl;
+    cout << endl << "Total bill: $" << bill.totalCost << endl;
     cout << "List of call charges:" << endl;
     for(unsigned int i=0; i < bill.callCosts.size(); i++)
         cout << "\t" << i+1 << ") $" << bill.callCosts[i] << endl;
